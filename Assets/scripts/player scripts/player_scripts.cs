97258fe0bdd1eb4fd8b96fbb367bebe24900f757
ibbitiminecraft <khadijakanwal90@gmail.com>
2026-06-23 15:29:09 -0500

    tracks are added

--- Assets/scripts/player scripts/player_scripts.cs
+++ Assets/scripts/player scripts/player_scripts.cs
@@ -1,7 +1,11 @@
+
 using UnityEngine;
 
 public class player_scripts : MonoBehaviour
 {
+
+    [SerializeField]
+    float move_speed = 5f;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -11,6 +15,6 @@ public class player_scripts : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        transform.Translate(Vector3.forward * Time.deltaTime * move_speed, Space.World);
     }
 }
