2e9d1c3d7ed8f47a07ab179850e6e214d4f4a634
ibbitiminecraft <khadijakanwal90@gmail.com>
2026-07-02 15:57:41 -0500

    track

--- Assets/scripts/player scripts/player_scripts.cs
+++ Assets/scripts/player scripts/player_scripts.cs
@@ -14,6 +14,8 @@ public class player_scripts : MonoBehaviour
     bool isSideMoving;
     [SerializeField]
     int sideSpeed;
+    float xpos;
+    float trackNumber;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -22,6 +24,7 @@ public class player_scripts : MonoBehaviour
         current_track = "middle";
         isSideMoving = false;
         sideSpeed = 5;
+        trackNumber = 0;
 
 
     }
@@ -29,14 +32,25 @@ public class player_scripts : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        xpos = transform.position.x;
         transform.Translate(Vector3.forward * Time.deltaTime * move_speed, Space.World);
         if (isSideMoving == true && move_direction == "left")
         {
             transform.Translate(Vector3.left * Time.deltaTime * sideSpeed, Space.World);
+            if (xpos <= trackNumber)
+            {
+                isSideMoving = false;
+                transform.position = new Vector3(trackNumber, 1, transform.position.z);
+            }
         }
         if (isSideMoving == true && move_direction == "right")
         {
             transform.Translate(Vector3.right * Time.deltaTime * sideSpeed, Space.World);
+            if (xpos >= trackNumber)
+            {
+                isSideMoving = false;
+                transform.position = new Vector3(trackNumber, 1, transform.position.z);
+            }
         }
     }
     public void onClickLeft()
@@ -46,12 +60,14 @@ public class player_scripts : MonoBehaviour
             move_direction = "left";
             current_track = "left";
             isSideMoving = true;
+            trackNumber = -1;
         }
         if (current_track == "right")
         {
             move_direction = "left";
             current_track = "middle";
             isSideMoving = true;
+            trackNumber = 0;
         }
     }
     public void onClickRight()
@@ -61,12 +77,14 @@ public class player_scripts : MonoBehaviour
             move_direction = "right";
             current_track = "right";
             isSideMoving = true;
+            trackNumber = 1;
         }
         if (current_track == "left")
         {
             move_direction = "right";
             current_track = "middle";
             isSideMoving = true;
+            trackNumber = 0;
         }
     }
 }
