784311715c87687bcfa5a0977bcd126460dd43b9
ibbitiminecraft <khadijakanwal90@gmail.com>
2026-07-15 15:55:47 -0500

    animations

--- Assets/scripts/coin scripts/coin script.cs
+++ Assets/scripts/coin scripts/coin script.cs
@@ -5,12 +5,25 @@ public class coinscript : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.Rotate(0,2,0, Space.World);
+        transform.Rotate(0, 2, 0, Space.World);
+    }
+    //     void OnTriggerEnter(Collider other)
+    //     {
+    //         this.gameObject.GetComponent<Animator>().Play("coin shrink animations");
+    //     }
+    private void OnTriggerEnter(Collider other)
+    {
+    Animator animator = GetComponent<Animator>();
+
+    if (animator != null)
+    {
+        animator.Play("coin shrink animations");
+    }
     }
 }
